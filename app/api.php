6e0d148cb7ab24440df6f182fcd70c97ec3a880c
Nikolai <n.sokolov@sxope.com>
2023-04-22 04:55:33 +0300

    Fix(api) return completed tasks

--- app/api.php
+++ app/api.php
@@ -13,6 +13,8 @@ $blank_todo = [
  * @return array
  */
 function listTodo(): array {
+    return read_data();
+//  return json_decode(file_get_contents('./data.json'), true);
 }
 
 /**
@@ -22,6 +24,13 @@ function listTodo(): array {
  * @return mixed
  */
 function createTodo (mixed $todoData): mixed {
+    global $blank_todo;
+    $all_todos = read_data();
+    $new_todo = array_merge($blank_todo, $todoData);
+    $new_todo['id'] = uniqid();
+    $all_todos[] = $new_todo;
+    write_data($all_todos);
+    return $new_todo;
 }
 
 /**
@@ -32,6 +41,13 @@ function createTodo (mixed $todoData): mixed {
  * @return mixed
  */
 function editTodo($todoId, $todoData) {
+    $all_todos = read_data();
+    $current_id = array_search($todoId, array_column($all_todos, 'id'));
+    $current_todo = $all_todos[$current_id];
+    $changed_todo = array_merge($current_todo, $todoData);
+    $all_todos[$current_id] = $changed_todo;
+    write_data($all_todos);
+    return $changed_todo;
 }
 
 /**
@@ -41,7 +57,13 @@ function editTodo($todoId, $todoData) {
  * @return void
  */
 function readTodo($todoId) {
-
+    $all_todos = read_data();
+    $current_id = array_search($todoId, array_column($all_todos, 'id'));
+    $current_todo = $all_todos[$current_id];
+    if (array_key_exists($current_id, $all_todos)) {
+     return $current_todo;
+    }
+    return false;
 }
 
 
@@ -52,4 +74,13 @@ function readTodo($todoId) {
  * @return bool
  */
 function deleteTodo($todoId) {
+    $all_todos = read_data();
+    $current_id = array_search($todoId, array_column($all_todos, 'id'));
+
+    if ($current_id !== false)
+    {
+        unset($all_todos[$current_id]);
+        write_data($all_todos);
+        return true;
+    } else {return false;}
 }
